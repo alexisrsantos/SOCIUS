7f5dee52738ebde698ecf384c82fc60bc5dc23c9
Alexis R. Santos-Lozada <appdemography@gmail.com>
2021-05-09 20:31:57 -0400

    Update Replication_GitHub.R

--- Replication_GitHub.R
+++ Replication_GitHub.R
@@ -53,7 +53,8 @@ county_full$sf_mortrate<-county_full$deaths/county_full$pop_under1_sf
 county_full$dp_mortrate<-county_full$deaths/county_full$pop_under1_dp
 
 #Rate Ratio
-county_full$rr<-(county_full$sf_mortrate)/county_full$dp_mortrate*100
+county_full$rr<-(county_full$sf_mortrate)/county_full$dp_mortrate
+county_full$rr2<-(county_full$sf_mortrate-county_full$dp_mortrate)*100
 
 county_full$rrbreaks<-ifelse(county_full$rr<97.50,"< 98.00",
                              ifelse(county_full$rr<=102.50,"98.00,102.50",
@@ -86,15 +87,20 @@ county_full3<-subset(county_full,is.na(county_full$rr)==FALSE)
 
 is.na(county_full3$dp_mortrate)<-sapply(county_full3$dp_mortrate, is.infinite)
 
-a<-ggplot(county_full3, aes(x=(sf_mortrate), y=(dp_mortrate),color=metro))+ geom_point(size=3)+
+a<-ggplot(county_full3, aes(x=(sf_mortrate), y=(dp_mortrate),color=metro))+ geom_point(size=3,alpha=0.5)+
   geom_abline(intercept = 0, size=0.25, slope = 1,color="blue")+theme_classic()+
   labs(title="2010 Infant Mortality Rates, with different denominators",
        subtitle="Blue line is the line of equality",size=4)+
-  ylab("IMR using Differential Privacy Denominator")+
-  xlab("IMR using 2010 U.S. Census Denominator")+theme_classic()+
+  ylab("Infant Mortality Rate using Differential Privacy Denominator")+
+  xlab("Infant Mortality Rate using 2010 U.S. Census Denominator")+theme_classic()+
   ylim(ymin,ymax)+
   xlim(xmin,xmax)+theme(legend.title =element_blank(),legend.position = c(0.8,0.5),
-                        legend.text=element_text(size=10))
+                        legend.text=element_text(size=10))+
+  annotate(geom="text",x=0.01,y=0.04,label="Overestimates",color="black")+
+  annotate(geom="text",x=0.04,y=0.00,label="Underestimates",color="black")+
+  annotate(geom="text",x=0.04,y=0.01,label="Equal Rates",color="black")+
+  annotate(geom="curve",x=0.04,y=0.0110,xend=0.03,yend=0.03,color="blue",
+           arrow=arrow(angle=20))
 
 a
 
@@ -104,22 +110,31 @@ county_full$categories<-ifelse(county_full$sf_mortrate==county_full$dp_mortrate,
 
 table(county_full$categories)
 
-
 #Panel B
 county_full2<-subset(county_full,is.na(county_full$rr)==FALSE)
 
-b<-ggplot(county_full2, aes(x=log(pop_under1_sf), y=rr,color=metro)) + 
-  geom_point(size=3)+
-  labs(title="Infant Mortality Rate Ratio by population size",
-       subtitle="± 2.5% ranges in black dashed lines",size=5)+
-  ylab("Mortality Rate Ratio")+xlab("Logarithm of the population under 1 year")+
-  theme_classic()+
-  ylim(25,175)+
-  xlim(4.5,13)+theme(legend.title =element_blank(),legend.position = "bottom",
-                     legend.text=element_text(size=10))+
-  geom_hline(yintercept=97.5, linetype="dashed", color = "black", size=0.5)+
-  geom_hline(yintercept=102.5, linetype="dashed", color = "black", size=0.5)
+breaks<-c(100,1000,10000,100000)
 
+labels<-c("100","1,000","10,000","100,000")
+
+b<-ggplot(county_full2, aes(x=(pop_under1_sf), y=rr,color=metro)) + 
+  geom_point(size=3,alpha=0.50)+
+  labs(title="Infant Mortality Rate Ratio by population size",
+       subtitle="Â± 0.025 error ranges in black dashed lines (equivalent to 5% error)",size=5)+
+  ylab("Relative difference between rates produced using both denominators, 1 = Equal Rates")+xlab("Population under 1 year")+
+  theme_classic()+
+  ylim(.25,1.75)+
+  theme(legend.title =element_blank(),legend.position = "bottom",
+                     legend.text=element_text(size=10))+
+  scale_x_log10(breaks=breaks,labels=labels)+
+  geom_hline(yintercept=0.975, linetype="dashed", color = "black", size=0.5)+
+  geom_hline(yintercept=1.025, linetype="dashed", color = "black", size=0.5)+
+  annotate(geom="text",x=15000,y=0.45,label="Summary File Rate is lower",color="black")+
+  annotate(geom="text",x=25000,y=1.65,label="Summary File Rate is higher",color="black")+
+  annotate(geom="curve",x=75000,y=0.75,xend=100000,yend=1,color="blue",
+           arrow=arrow(angle=20))+
+  annotate(geom="text",x=35000,y=0.75,label="Equal Rates",color="black")
+  
 b
 
 #Create final Figure (Join Panels)
